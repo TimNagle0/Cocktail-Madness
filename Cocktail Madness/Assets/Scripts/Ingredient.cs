be622369aa1b01da4a5adcebddf97da01aa53f84
TimNagle0 <timnagle0@gmail.com>
2022-01-05 13:51:39 +0100

    Moved to fully digital controls
    - Removed Firebase integration
    - Added hotkeys for ingredients, shaker and serving

--- Cocktail Madness/Assets/Scripts/Ingredient.cs
+++ Cocktail Madness/Assets/Scripts/Ingredient.cs
@@ -5,21 +5,33 @@ using System;
 
 public class Ingredient : MonoBehaviour
 {
+    [SerializeField] public KeyCode hotkey;
+
+    // Events for different actions for data collection,
+    // OnUse is also used for adding ingredients to the shaker
     public event Action<GameObject> OnSelect;
     public event Action<GameObject> OnDeselect;
     public event Action<GameObject> OnUse;
 
-    private Vector3 startPosition;
     private Collider2D col;
     private AudioSource audioSource;
 
-    public bool isSelected = false;
-    // Start is called before the first frame update
     void Start()
     {
         col = GetComponent<Collider2D>();
         audioSource = GetComponent<AudioSource>();
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(hotkey))
+        {
+            OnUse(gameObject);
+        }
+    }
+    
+    // Code for adding ingredients by dragging them to the shaker.
+    // Also detects if a player picks up an ingredient but drops is again for data collection
     private void OnMouseDown()
     {
         OnSelect(gameObject);
@@ -39,9 +51,8 @@ public class Ingredient : MonoBehaviour
             }
 
         }
-        OnDeselect(gameObject);
-        
-    }
+        OnDeselect(gameObject);  
+    } 
 
     public void PlaySound()
     {
